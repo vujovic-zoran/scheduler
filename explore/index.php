248cbc82437b2ed113793753c8166a7e16e21725
zoran <zoran@zoran-pc>
2013-03-26 08:21:40 +0100

    bug fixed

---
+++ explore/index.php
@@ -0,0 +1,15 @@
+<?php
+require '../classes/uFlex.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+		<?php
+		// put your code here
+		?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require 'classes/DB.php';
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+		<h1>Home Page</h1>
+		<a href="explore">Explore</a>
+    </body>
+</html>
